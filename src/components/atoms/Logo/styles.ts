f19aef9236362190f480caf310ac9e1718de7add
Kaluã Bentes <kaluanbentes@gmail.com>
2020-10-30 22:30:55 -0300

    Use utils styled

--- src/components/atoms/Logo/styles.ts
+++ src/components/atoms/Logo/styles.ts
@@ -1,7 +1,8 @@
-import styled from 'styled-components'
+import { css } from 'styled-components'
+import styled from '../../../styles/utils/styled'
 
-export const Image = styled.img`
+export const Image = styled('img')(css`
   height: 150px;
   width: 150px;
   border-radius: 30px;
-`
+`)
