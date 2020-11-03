e5082457b36436f210ad647bc57c485330359f46
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-03 00:25:09 -0300

    Rename method

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,17 @@ import { useRouter } from 'next/dist/client/router'
 
 import { logoSrc } from '../components/atoms/Logo'
 import Home, { coverSrc } from '../components/templates/Home'
-import getTranslation from '../i18n/getTranslation'
+import getString from '../i18n/getString'
 
 function HomePage() {
   const router = useRouter()
 
   return (
     <Home
-      title={getTranslation('app.home.title')}
-      text={getTranslation('app.home.text')}
-      enterLabel={getTranslation('app.home.enterLabel')}
-      verifyLabel={getTranslation('app.home.verifyLabel')}
+      title={getString('app.home.title')}
+      text={getString('app.home.text')}
+      enterLabel={getString('app.home.enterLabel')}
+      verifyLabel={getString('app.home.verifyLabel')}
       coverSrc={coverSrc}
       logoSrc={logoSrc}
       onEnter={() => router.push('/login')}
