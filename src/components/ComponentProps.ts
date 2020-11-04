894b32d184bf2264bddd59aa5d7c0cf04385ce07
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-04 00:34:59 -0300

    Add padding props

--- src/components/ComponentProps.ts
+++ src/components/ComponentProps.ts
@@ -2,6 +2,7 @@ import React from 'react'
 
 export interface ComponentProps {
   margin?: string
+  padding?: string
   children?: React.ReactNode
   align?: 'center' | 'right' | 'left' | 'justify'
   className?: string
