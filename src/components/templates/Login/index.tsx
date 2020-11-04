b1b7e031d4cdde6173b4ce7f8037c724f45c7243
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-03 23:57:59 -0300

    Add outer container

--- src/components/templates/Login/index.tsx
+++ src/components/templates/Login/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import getString from '../../../i18n/getString'
 
 import filterNumber from '../../../utils/filters/filterNumber'
 import formatPhone from '../../../utils/formatters/formatPhone'
+
 import Button from '../../atoms/Button'
 import Heading from '../../atoms/Heading'
 import Input from '../../atoms/Input'
 import Paragraph from '../../atoms/Paragraph'
 
-import { Container, CustomIconButton as IconButton } from './styles'
+import {
+  Container,
+  CustomIconButton as IconButton,
+  OuterContainer
+} from './styles'
 
 interface LoginProps {
   title: string
   text: string
   advanceLabel: string
   phoneLabel: string
-  onAdvance: (phone: string) => void
+  error?: string
+  onAdvance: (phone) => void
   onBack: () => void
 }
 
@@ -24,49 +29,40 @@ function Login({
   text,
   advanceLabel,
   phoneLabel,
+  error,
   onAdvance,
   onBack
 }: LoginProps) {
   const [phone, setPhone] = React.useState('')
-  const [error, setError] = React.useState('')
-
-  function handlePhoneChange(event) {
-    setPhone(formatPhone(event.target.value))
-  }
-
-  function handleAdvance() {
-    if (phone.length < 15) {
-      setError(getString('app.login.phoneError'))
-      return
-    }
-
-    setError('')
-    onAdvance(filterNumber(phone))
-  }
 
   return (
-    <Container>
-      <IconButton margin="0 0 30px 0" name="arrow_back" onClick={onBack} />
-      <Heading size="large" margin="0 0 10px 0">
-        {title}
-      </Heading>
-      <Paragraph margin="0 0 20px 0" variant="muted">
-        {text}
-      </Paragraph>
-      <Input
-        name="phone"
-        value={phone}
-        onChange={handlePhoneChange}
-        margin="0 0 20px 0"
-        label={phoneLabel}
-        maxLength="15"
-        placeholder="(00) 00000-0000"
-        error={error}
-      />
-      <Button variant="primary" onClick={handleAdvance}>
-        {advanceLabel}
-      </Button>
-    </Container>
+    <OuterContainer>
+      <Container>
+        <IconButton margin="0 0 30px 0" name="arrow_back" onClick={onBack} />
+        <Heading size="large" margin="0 0 10px 0">
+          {title}
+        </Heading>
+        <Paragraph margin="0 0 20px 0" variant="muted">
+          {text}
+        </Paragraph>
+        <Input
+          name="phone"
+          value={phone}
+          onChange={event => setPhone(formatPhone(event.target.value))}
+          margin="0 0 20px 0"
+          label={phoneLabel}
+          maxLength="15"
+          placeholder="(00) 00000-0000"
+          error={error}
+        />
+        <Button
+          variant="primary"
+          onClick={() => onAdvance(filterNumber(phone))}
+        >
+          {advanceLabel}
+        </Button>
+      </Container>
+    </OuterContainer>
   )
 }
 
