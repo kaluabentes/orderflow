5e89c3564e86c44cbe85718f894fbb99d97146a7
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-03 23:58:11 -0300

    Add outer container styles

--- src/components/templates/Login/styles.ts
+++ src/components/templates/Login/styles.ts
@@ -7,10 +7,22 @@ export const Container = styled('div')(css`
   width: 100%;
   margin: 0 auto;
   padding: 20px;
+  background: white;
 
   @media (min-width: 600px) {
     max-width: 400px;
-    padding-bottom: 100px;
+    margin-bottom: 100px;
+    padding: 40px;
+    border-radius: 7px;
+  }
+`)
+
+export const OuterContainer = styled('div')(css`
+  @media (min-width: 600px) {
+    display: flex;
+    align-items: center;
+    height: 100vh;
+    background: ${props => props.theme.colors.background};
   }
 `)
 
