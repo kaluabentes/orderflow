a82dfb7e1598deb65471be7b8abeb87131e5d4ab
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-03 23:59:11 -0300

    Add login content page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,34 @@
 import React from 'react'
 
-export default function Login() {
-  return <h1>Login</h1>
+import { useRouter } from 'next/router'
+import getString from '../i18n/getString'
+import Login from '../components/templates/Login'
+
+export default function LoginPage() {
+  const router = useRouter()
+  const [error, setError] = React.useState('')
+
+  function handleAdvance(phone) {
+    if (phone.length < 15) {
+      setError(getString('app.login.phoneError'))
+      return
+    }
+
+    setError('')
+    // 1. Store in the context
+    // 2. Send to backend
+    router.push('/verify')
+  }
+
+  return (
+    <Login
+      title={getString('app.login.title')}
+      text={getString('app.login.text')}
+      onBack={() => router.push('/')}
+      onAdvance={handleAdvance}
+      advanceLabel="Avançar"
+      phoneLabel="Celular"
+      error={error}
+    />
+  )
 }
