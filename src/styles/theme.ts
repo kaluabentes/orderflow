6491fd0e961e4983c3d5a53470b74e13083575a3
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-03 23:59:24 -0300

    Add background color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,7 +4,8 @@ export default {
     info: '#3858cd',
     warning: '#fe932a',
     text: '#000000',
-    textMuted: 'rgba(0, 0, 0, 0.4)'
+    textMuted: 'rgba(0, 0, 0, 0.4)',
+    background: '#f7f7fa'
   },
   typography: {
     fontFamily: "'Poppins', sans-serif",
