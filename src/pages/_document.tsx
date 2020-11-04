5d52d2d0003988f36e329249c60c48dda80279b6
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-03 23:58:56 -0300

    Add font link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,6 +45,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
