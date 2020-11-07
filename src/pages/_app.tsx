32b3b8545c616d0fbc683c360bd612ea0f4b4c62
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-07 01:14:07 -0300

    Add sentry

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
+
+Sentry.init({
+  dsn:
+    'https://99e75d05cc5d4bfa89db557158780c18@o447297.ingest.sentry.io/5507779',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0
+})
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
