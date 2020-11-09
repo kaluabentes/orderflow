067e6ccd0367f2f098ef157fa679311c4fac5bbc
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-09 00:34:27 -0300

    Add auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { Integrations } from '@sentry/tracing'
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
+import { AuthProvider } from '../modules/auth/context'
 
 Sentry.init({
   dsn:
@@ -17,8 +18,10 @@ Sentry.init({
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
     </ThemeProvider>
   )
 }
