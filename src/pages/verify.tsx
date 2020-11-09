0b19a1b12156b8480a0b4b4d56df2ddcc04f9231
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-09 00:37:02 -0300

    Create verify page

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -1,5 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import { get } from 'lodash'
 
-export default function Verify() {
-  return <h1>Verify</h1>
+import Verify from '../components/templates/Verify'
+import getString from '../i18n/getString'
+import { useAuth } from '../modules/auth/context'
+import formatPhone from '../utils/formatters/formatPhone'
+import { checkCode } from '../modules/auth/actions'
+
+function VerifyPage() {
+  const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+  const [auth, dispatch] = useAuth()
+  const phone = get(auth, 'user.phone', '')
+  const name = get(auth, 'user.name', '')
+
+  async function handleAdvance(code) {
+    if (code.length < 4) {
+      setError(getString('app.verify.codeError'))
+      return
+    }
+
+    setError('')
+    setIsLoading(true)
+
+    try {
+      await checkCode(dispatch, phone, code)
+
+      if (!name) {
+        router.push('/register')
+        return
+      }
+
+      router.push('/menu')
+    } catch (error) {
+      const { code } = error.response.data.error
+
+      if (code === 20404) {
+        setError(getString('app.verify.codeError'))
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <Verify
+      error={error}
+      title="Verificar"
+      text={
+        <>
+          {`${getString('app.verify.text')} `}
+          <strong>{formatPhone(phone)}</strong>
+        </>
+      }
+      onBack={() => router.push('/login')}
+      onAdvance={handleAdvance}
+      advanceLabel={getString('app.verify.advanceLabel')}
+      codeLabel={getString('app.verify.codeLabel')}
+      isLoading={isLoading}
+    />
+  )
 }
+
+export default VerifyPage
