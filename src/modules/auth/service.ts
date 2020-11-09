1c2b46ea7a8c810dbc0253ef7aa2eec93a4b4d67
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-09 00:34:19 -0300

    Add service

--- src/modules/auth/service.ts
+++ src/modules/auth/service.ts
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
-export function sendVerificationCode(phone) {
+export function login(phone) {
   return axios.post('/api/login', { phone })
+}
+
+export function verify(phone, code) {
+  return axios.post('/api/verify', { phone, code })
 }
