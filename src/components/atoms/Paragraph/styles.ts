17decf83d3bfd27f5188cbf58ca5413822a1d3c8
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-13 23:24:24 -0300

    Change font weight

--- src/components/atoms/Paragraph/styles.ts
+++ src/components/atoms/Paragraph/styles.ts
@@ -4,6 +4,7 @@ import styled from '../../../styles/utils/styled'
 export const Container = styled('p')(css`
   margin: 0;
   line-height: 1.4em;
+  font-weight: 300;
 
   ${props => css`
     font-size: ${props.theme.typography.size[props.size]};
