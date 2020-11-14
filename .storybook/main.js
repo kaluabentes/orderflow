75e8173c2b36bb0bdd74b8f36c7f81210eb9aed9
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-14 16:47:00 -0300

    Add path alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,19 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*stories.mdx', '../src/**/*stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async config => {
+    const customConfig = {
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '~': path.resolve(__dirname, '..', 'src')
+        }
+      }
+    }
+
+    return { ...config, ...customConfig }
+  }
 }
