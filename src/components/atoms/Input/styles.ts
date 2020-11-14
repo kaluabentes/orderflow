94e5c276767b29e3f0dc32c5477b092b21760eb2
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-13 23:24:04 -0300

    Change border radius

--- src/components/atoms/Input/styles.ts
+++ src/components/atoms/Input/styles.ts
@@ -11,20 +11,18 @@ export const Field = styled('input')(css`
   height: 44px;
   padding: 0 10px;
   border: 1px solid rgba(0, 0, 0, 0.2);
-  border-radius: 7px;
+  border-radius: 6px;
   width: 100%;
   display: block;
 
   &:focus {
     outline: none;
-    box-shadow: 0px 0px 0px 1px inset ${props => props.theme.colors.info};
     border-color: ${props => props.theme.colors.info};
   }
 
   ${props =>
     props.hasError &&
     css`
-      box-shadow: 0px 0px 0px 1px inset ${props => props.theme.colors.primary};
       border-color: ${props => props.theme.colors.primary};
     `}
 `)
