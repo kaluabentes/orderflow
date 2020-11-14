06e9d6c58c4698fc009b4719c6a167dd2dd960df
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-13 23:24:41 -0300

    Rename template

--- src/components/templates/BackNavPage/styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from 'styled-components'
-
-import styled from '../../../styles/utils/styled'
-import IconButton from '../../atoms/IconButton'
-
-export const CustomIconButton = styled(IconButton)(css`
-  transform: translate(-12px, -12px);
-  display: block;
-`)
