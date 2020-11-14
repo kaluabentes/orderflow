be6b25c2e32a3317c1c7189ff71ce49003610fbf
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-14 18:20:33 -0300

    Add checkToken method

--- src/server/auth/AuthService.ts
+++ src/server/auth/AuthService.ts
@@ -1,8 +1,29 @@
 import jwt from 'jsonwebtoken'
 
+interface JwtPayload {
+  sub: string
+}
+
 class AuthService {
   static createToken(payload) {
     return jwt.sign(payload, process.env.JWT_SECRET)
+  }
+
+  static checkToken(token): Promise<JwtPayload | boolean> {
+    if (!token) {
+      return Promise.resolve(false)
+    }
+
+    return new Promise(resolve => {
+      jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+        if (error) {
+          resolve(false)
+          return
+        }
+
+        resolve(user)
+      })
+    })
   }
 }
 
