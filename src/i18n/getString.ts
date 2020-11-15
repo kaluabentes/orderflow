ebc7a59c210f929ada1a7284a208ae848626e908
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-14 22:17:57 -0300

    Add support to params

--- src/i18n/getString.ts
+++ src/i18n/getString.ts
@@ -4,8 +4,20 @@ const translationMap = {
   pt
 }
 
-function getString(key) {
-  return translationMap[process.env.LOCALE][key]
+function getString(key, params?) {
+  const string = translationMap[process.env.LOCALE][key]
+
+  if (params) {
+    let newString
+
+    Object.keys(params).forEach(key => {
+      newString = string.replaceAll(`\$\{${key}\}`, params[key])
+    })
+
+    return newString
+  }
+
+  return string
 }
 
 export default getString
