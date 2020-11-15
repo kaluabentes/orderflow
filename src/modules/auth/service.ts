acd37b161a50857d5f75fb094b3f5da5f453e395
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-14 22:18:38 -0300

    Add register method

--- src/modules/auth/service.ts
+++ src/modules/auth/service.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import buildAuthHeader from '~/utils/buildAuthHeader'
 
 export function login(phone) {
   return axios.post('/api/login', { phone })
@@ -6,4 +7,8 @@ export function login(phone) {
 
 export function verify(phone, code) {
   return axios.post('/api/verify', { phone, code })
+}
+
+export function register(token, data) {
+  return axios.post('/api/register', data, buildAuthHeader(token))
 }
