77a7abfadda131b17d970a72896bc36204d75d1e
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-15 20:02:01 -0300

    Add avalaibility page content

--- src/pages/availability.tsx
+++ src/pages/availability.tsx
@@ -1,5 +1,25 @@
+import { useRouter } from 'next/router'
 import React from 'react'
+import Availability from '~/components/templates/Availability'
 
-export default function Availability() {
-  return <h1>Availability</h1>
+function AvailabilityPage() {
+  const router = useRouter()
+
+  return (
+    <Availability
+      onBack={() => router.push('/')}
+      districts={[
+        {
+          id: '1',
+          name: 'Campeche'
+        },
+        {
+          id: '2',
+          name: 'Flores'
+        }
+      ]}
+    />
+  )
 }
+
+export default AvailabilityPage
