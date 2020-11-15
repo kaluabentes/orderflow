4ac0fbc65755fa4f00b21157d09245833b5a6dba
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-14 22:20:05 -0300

    Create register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
+
+import Register from '~/components/templates/Register'
+import useAuth from '~/modules/auth/hook'
+import { register } from '~/modules/auth/service'
+import validate from '~/utils/validation/validate'
+
+const INITIAL_ERRORS = {
+  name: null,
+  district: null,
+  street: null,
+  number: null
+}
+
+const DataSchema = {
+  name: ['required'],
+  district: ['required'],
+  street: ['required'],
+  number: ['required']
+}
 
 function RegisterPage() {
-  return <h1>Register</h1>
+  const [errors, setErrors] = useState<any>(INITIAL_ERRORS)
+  const router = useRouter()
+  const [auth] = useAuth()
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function handleSubmit(data) {
+    const validation = validate(DataSchema, data)
+
+    if (!validation.isValid) {
+      setErrors(validation.error)
+      return
+    }
+
+    setErrors(INITIAL_ERRORS)
+    setIsLoading(true)
+    await register(auth.token, data)
+    router.push('/menu')
+  }
+
+  return (
+    <Register
+      isLoading={isLoading}
+      errors={errors}
+      onBack={() => router.push('/verify')}
+      onSubmit={handleSubmit}
+      districts={[
+        {
+          id: '1',
+          name: 'Campeche'
+        },
+        {
+          id: '2',
+          name: 'Flores'
+        }
+      ]}
+    />
+  )
 }
 
 export default RegisterPage
