4b64958b101eccd68630e70e56188f7232d265df
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-14 22:18:18 -0300

    Change to use local storage state

--- src/modules/auth/actions.ts
+++ src/modules/auth/actions.ts
@@ -1,11 +1,25 @@
-import { login, verify } from './service'
+import Storage from '~/utils/services/Storage'
 
-export async function makeLogin(dispatch, phone) {
+import { login, register, verify } from './service'
+
+export async function makeLogin(setState, phone) {
   await login(phone)
-  dispatch({ user: { phone } })
+
+  setState({
+    user: { phone }
+  })
 }
 
-export async function checkCode(dispatch, phone, code) {
+export async function checkCode(setState, phone, code) {
   const response = await verify(phone, code)
-  dispatch(response)
+
+  setState(response.data)
+
+  return response.data.user
+}
+
+export async function registerUser(setState, token, data) {
+  const response = await register(token, data)
+
+  setState(response.data)
 }
