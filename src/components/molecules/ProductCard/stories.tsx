a902a42f3bfbf91830b44746cf71835595d97af8
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-19 22:22:17 -0300

    Move sample props

--- src/components/molecules/ProductCard/stories.tsx
+++ src/components/molecules/ProductCard/stories.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import StoryContainer from '~/components/utils/StoryContainer'
 
-import ProductCard from '.'
+import ProductCard, { cardProps } from '.'
 
 export default {
   title: 'Molecules/ProductCard',
   component: ProductCard
-}
-
-const cardProps = {
-  image:
-    'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food-1024x683.jpg',
-  title: 'Frango a milanesa com delicioso e cocrante empanado',
-  description:
-    'Um delicioso frango frito empanado com molho especial da casa servi 5 pessoas',
-  price: 15.5
 }
 
 export const Default = () => (
