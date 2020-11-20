9c54231d908415d502237631c37edaa60ddfd414
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-19 22:22:58 -0300

    Set width size

--- src/components/utils/StoryContainer.ts
+++ src/components/utils/StoryContainer.ts
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components'
 const StoryContainer = styled.div`
   padding: 15px;
   height: 100vh;
-  width: 100vw;
+  width: 100%;
 
   ${props =>
     props.background &&
