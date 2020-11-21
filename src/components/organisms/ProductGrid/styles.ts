5dc81f46dd8401554c65f94b268d02f2965561f5
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-20 21:58:56 -0300

    Set spacing size

--- src/components/organisms/ProductGrid/styles.ts
+++ src/components/organisms/ProductGrid/styles.ts
@@ -5,7 +5,8 @@ import styled from '~/styles/utils/styled'
 export const Container = styled('section')(css`
   max-width: ${props => props.theme.layout.maxWidth};
   width: 100%;
-  margin: 0 auto;
+  margin: 0 auto 40px auto;
+  padding: 20px;
 `)
 
 export const Title = styled('h3')(css`
