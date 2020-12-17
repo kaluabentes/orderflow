66f58192e1dcd0fb6e5ecbe67ce67b5a4aab2dfe
Kaluã Bentes <kaluanbentes@gmail.com>
2020-12-16 21:49:11 -0400

    Add categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,73 @@ import Home from '~/components/templates/Home'
 import useAuth from '~/modules/auth/hooks/useAuth'
 
 const products = [
-  productProps,
-  productProps,
-  productProps,
-  productProps,
-  productProps,
-  productProps
+  {
+    id: 1,
+    image:
+      'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food-1024x683.jpg',
+    title: 'Frango a milanesa com delicioso e cocrante empanado',
+    description:
+      'Um delicioso frango frito empanado com molho especial da casa servi 5 pessoas',
+    price: 15.5
+  },
+  {
+    id: 2,
+    image:
+      'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food-1024x683.jpg',
+    title: 'Frango a milanesa com delicioso e cocrante empanado',
+    description:
+      'Um delicioso frango frito empanado com molho especial da casa servi 5 pessoas',
+    price: 15.5
+  },
+  {
+    id: 3,
+    image:
+      'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food-1024x683.jpg',
+    title: 'Frango a milanesa com delicioso e cocrante empanado',
+    description:
+      'Um delicioso frango frito empanado com molho especial da casa servi 5 pessoas',
+    price: 15.5
+  },
+  {
+    id: 4,
+    image:
+      'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food-1024x683.jpg',
+    title: 'Frango a milanesa com delicioso e cocrante empanado',
+    description:
+      'Um delicioso frango frito empanado com molho especial da casa servi 5 pessoas',
+    price: 15.5
+  },
+  {
+    id: 5,
+    image:
+      'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food-1024x683.jpg',
+    title: 'Frango a milanesa com delicioso e cocrante empanado',
+    description:
+      'Um delicioso frango frito empanado com molho especial da casa servi 5 pessoas',
+    price: 15.5
+  },
+  {
+    id: 6,
+    image:
+      'https://mercadoeconsumo.com.br/wp-content/uploads/2019/04/Que-comida-saud%C3%A1vel-que-nada-brasileiro-gosta-de-fast-food-1024x683.jpg',
+    title: 'Frango a milanesa com delicioso e cocrante empanado',
+    description:
+      'Um delicioso frango frito empanado com molho especial da casa servi 5 pessoas',
+    price: 15.5
+  }
+]
+
+const categories = [
+  {
+    id: 1,
+    name: 'Hamburguers',
+    products
+  },
+  {
+    id: 2,
+    name: 'Hamburguers',
+    products
+  }
 ]
 
 function HomePage() {
@@ -35,7 +96,7 @@ function HomePage() {
 
   return (
     <Home
-      products={products}
+      categories={categories}
       logoSrc={logoSrc}
       address="Servidão Vitórias, 40"
       userName={getUserName()}
