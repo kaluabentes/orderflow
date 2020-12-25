31ef72d2128c5551bf44b077d8d41381fbed80bb
Kaluã Bentes <kaluanbentes@gmail.com>
2020-12-25 19:35:45 -0400

    Format money

--- src/utils/formatters/formatMoney.ts
+++ src/utils/formatters/formatMoney.ts
@@ -1,10 +1,13 @@
 import getString from '~/i18n/getString'
 
 function formatMoney(value: number) {
-  return `${getString('app.currency')} ${value.toLocaleString('', {
-    maximumFractionDigits: 2,
-    minimumFractionDigits: 2
-  })}`
+  return `${getString('app.currency')} ${value.toLocaleString(
+    getString('app.locale'),
+    {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2
+    }
+  )}`
 }
 
 export default formatMoney
