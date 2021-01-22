867b5eea42cb7dad1c660bc5e6fc1108544574fc
Kaluã Bentes <kaluanbentes@gmail.com>
2021-01-22 11:08:22 -0400

    Remove page loader

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -2,16 +2,12 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/dist/client/router'
 
 import Welcome, { coverSrc } from '~/components/templates/Welcome'
-import usePageLoader from '~/components/organisms/PageLoader/usePageLoader'
-import PageLoader from '~/components/organisms/PageLoader'
 
 function WelcomePage() {
   const router = useRouter()
-  const isLoading = usePageLoader()
 
   return (
     <>
-      {isLoading && <PageLoader />}
       <Welcome
         title={process.env.STORE_NAME}
         coverSrc={coverSrc}
